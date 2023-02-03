039db19817d4ddd346c987a349abb35ec2b6b9e2
Matheus Kisiolar <matheuskisiolar@gmail.com>
2023-02-02 21:09:53 -0300

    create playRound function

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ function getComputerChoice() {
     let num = Math.random();
     let choice;
     if (num < 0.33) {
-        choice = "Rock";
+        choice = "rock";
     } else if (num < 0.66) {
-        choice = "Paper";
+        choice = "paper";
     } else {
-        choice = "Scissors";
+        choice = "scissors";
     }
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if (playerSelection === 'paper' && computerSelection === "rock" || playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log(`${playerSelection} beats ${computerSelection}! Player wins!`);
+    } else {
+        console.log(`${computerSelection} beats ${playerSelection}! Computer wins!`);
+    }
+}
+   
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
