c770e10eefda8f7ba331a5bb9989fe77f3b21949
Matheus Kisiolar <matheuskisiolar@gmail.com>
2023-02-02 21:55:11 -0300

    add game function

--- script.js
+++ script.js
@@ -16,12 +16,41 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("It's a tie!");
     } else if (playerSelection === 'paper' && computerSelection === "rock" || playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log(`${playerSelection} beats ${computerSelection}! Player wins!`);
+        console.log(`${playerSelection} beats ${computerSelection}! Player wins the round!`);
+        return true;
     } else {
-        console.log(`${computerSelection} beats ${playerSelection}! Computer wins!`);
+        console.log(`${computerSelection} beats ${playerSelection}! Computer wins the round!`);
+        return false;
     }
 }
-   
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    const rounds = +prompt("How many rounds do you want to play?");
+    let playerSelection;
+    let computerSelection;
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerWinsRound;
+    if (rounds === 0) {
+        alert("Game canceled.")
+        return;
+    }
+    console.log(`Game started! Rounds: ${rounds}`);
+    for (let i = 0; i < rounds; i ++) {
+        playerSelection = prompt("Do you choose rock, paper or scissors?");
+        computerSelection = getComputerChoice();
+        playerWinsRound = playRound(playerSelection, computerSelection)
+        if (playerWinsRound) {
+            playerScore++;
+        } else if (playerWinsRound === false) {
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log(`Player : ${playerScore} X ${computerScore} : Computer \nPlayer wins!`);
+    } else if (computerScore > playerScore) {
+        console.log(`Player : ${playerScore} X ${computerScore} : Computer \nComputer wins!`);     
+    } else {
+        console.log(`Player : ${playerScore} X ${computerScore} : Computer \nIt's a tie!`);            
+    }
+}
